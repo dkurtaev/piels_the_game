614f302d4d6c5329cb7c80a05b990dcf99170ec9
dkurtaev <dmitry.kurtaev@gmail.com>
2016-10-16 23:42:10 +0300

    Isometric top contour drawing

--- scripts/game_field.js
+++ scripts/game_field.js
@@ -1,13 +1,14 @@
 function gameField(canvasId) {
   // Game field dimensions in isometric points.
-  // Field example with width 4 and height 3:
-  //       **
-  //     **  **
+  // Field example with width 5 and height 3:
+  //         **
+  //       **  **
+  //     **      **
   //   **      **
   // **      **
   //   **  **
   //     **
-  // Here each asterisk - colored pixel. Total width in pixels - 12, height - 6.
+  // Here each asterisk - colored pixel. Total width in pixels - 14, height - 7.
   var FIELD_WIDTH = 100;
   var FIELD_HEIGHT = 100;
 
@@ -17,8 +18,8 @@ function gameField(canvasId) {
   }
 
   var canvas = document.getElementById(canvasId);
-  canvas.width = 2 * (FIELD_WIDTH + FIELD_HEIGHT - 1);
-  canvas.height = 2 * FIELD_HEIGHT;
+  canvas.height = FIELD_WIDTH + FIELD_HEIGHT - 1;
+  canvas.width = 2 * canvas.height;
 
   var gl = canvas.getContext("webgl", {antialias: false});
   if (!gl) {
@@ -27,42 +28,49 @@ function gameField(canvasId) {
   }
   this.gl = gl;
 
-  gl.clearColor(0.0, 0.8, 0.5, 1.0);
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   this.shaderProgram = createShaderProgram(gl);
 
-  try {
   gl.useProgram(this.shaderProgram);
 
   var vbo = gl.createBuffer();
-  var vertices = [0.0, 0.0, 1.0, 1.0];
+  var vertices = [0, FIELD_WIDTH - 1, 2 * FIELD_HEIGHT, canvas.height,
+                  2 * FIELD_HEIGHT, canvas.height - 1,
+                  canvas.width, FIELD_HEIGHT - 1,
+                  canvas.width - 2, FIELD_HEIGHT - 1,
+                  2 * (FIELD_WIDTH - 2), -1,
+                  2 * (FIELD_WIDTH - 1), 1,
+                  0, FIELD_WIDTH];
   gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
   var loc_position = gl.getAttribLocation(this.shaderProgram, "a_position");
+  var loc_projection =
+      gl.getUniformLocation(this.shaderProgram, "u_projection_matrix");
+
+  var proj_matrix = orthoM(0, canvas.width, canvas.height, 0);
+  gl.uniformMatrix4fv(loc_projection, false, new Float32Array(proj_matrix));
 
   gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
   gl.vertexAttribPointer(loc_position, 2, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(loc_position);
-  gl.drawArrays(gl.LINES, 0, 2);
+  gl.drawArrays(gl.LINES, 0, 8);
 
   gl.disableVertexAttribArray(loc_position);
-} catch (err) {
-  window.alert(err.message);
-}
-
 };
 
 function createShaderProgram(gl) {
   var vertexSrc =
     "attribute vec2 a_position;" +
+    "uniform mat4 u_projection_matrix;" +
     "void main() {" +
-      "gl_Position = vec4(a_position, 0.0, 1.0);" +
+      "gl_Position = u_projection_matrix * vec4(a_position, 0.0, 1.0);" +
     "}";
   var fragmentSrc =
     "void main() {" +
-      "gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);" +
+      "gl_FragColor = vec4(0.23, 0.09, 0.07, 1.0);" +
     "}";
 
   var shaderProgram = gl.createProgram();
@@ -93,4 +101,15 @@ function createShader(gl, type, src) {
     window.alert("Shader compilation error: " + gl.getShaderInfoLog(shader));
   }
   return shader;
+};
+
+function orthoM(left, right, bottom, top) {
+  // near = 0, far = 1.
+  var reverted_width = 1.0 / (right - left);
+  var reverted_height = 1.0 / (top - bottom);
+  return [2 * reverted_width, 0, 0, 0,
+          0, 2 * reverted_height, 0, 0,
+          0, 0, -2, 0,
+          -(right + left) * reverted_width, -(top + bottom) * reverted_height,
+          -1, 1];
 };
