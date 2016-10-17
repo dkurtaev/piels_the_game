edb4388658e10d6b0e8d14396a3527e9ae8788b3
dkurtaev <dmitry.kurtaev@gmail.com>
2016-10-17 23:11:38 +0300

    Shaders factory

--- scripts/game_field.js
+++ scripts/game_field.js
@@ -34,9 +34,6 @@ function gameField(canvasId) {
   }
   this.gl = gl;
 
-  gl.clearColor(1.0, 1.0, 1.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
   // Generate texture.
   var pixels = new Uint8Array(canvas.width * canvas.height * 3).fill(255);
 
@@ -44,6 +41,7 @@ function gameField(canvasId) {
   var upper_y = FIELD_WIDTH - 1, lower_y = upper_y;
   var upper_dy = -1, lower_dy = 1;
   for (var x = 0; x < canvas.width; x += 2) {
+    // Top.
     for (var y = upper_y; y <= lower_y; ++y) {
       var offset = (y * canvas.width + x) * 3;
       pixels[offset] = 59;
@@ -56,7 +54,7 @@ function gameField(canvasId) {
     if (upper_y == 0) {
       upper_dy = 1;
     }
-    if (lower_y == canvas.height - 1) {
+    if (lower_y == FIELD_WIDTH + FIELD_HEIGHT - 2) {
       lower_dy = -1;
     }
     upper_y += upper_dy;
@@ -71,9 +69,12 @@ function gameField(canvasId) {
                 gl.RGB, gl.UNSIGNED_BYTE, pixels);
   gl.bindTexture(gl.TEXTURE_2D, null);
 
-  this.shaderProgram = createShaderProgram(gl);
+  // Shader program setup.
+  var vertShader = document.getElementById("default_vert_shader").text;
+  var fragShader = document.getElementById("default_frag_shader").text;
+  this.shaderProgram = createShaderProgram(gl, vertShader, fragShader);
 
-  gl.useProgram(this.shaderProgram);
+  var loc_texture = gl.getUniformLocation(this.shaderProgram, "u_texture");
 
   // Vertices position VBO.
   var position_vbo = gl.createBuffer();
@@ -95,9 +96,8 @@ function gameField(canvasId) {
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
   gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-  var loc_position = gl.getAttribLocation(this.shaderProgram, "a_position");
-  var loc_tex_coords = gl.getAttribLocation(this.shaderProgram, "a_tex_coords");
-  var loc_texture = gl.getUniformLocation(this.shaderProgram, "u_texture");
+  // Drawing.
+  gl.useProgram(this.shaderProgram);
 
   gl.enable(gl.TEXTURE_2D);
   gl.bindTexture(gl.TEXTURE_2D, tex_id);
@@ -105,66 +105,19 @@ function gameField(canvasId) {
   gl.uniform1i(loc_texture, 0);
 
   gl.bindBuffer(gl.ARRAY_BUFFER, position_vbo);
-  gl.vertexAttribPointer(loc_position, 2, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(loc_position);
+  gl.vertexAttribPointer(Attrib.POSITION, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(Attrib.POSITION);
 
   gl.bindBuffer(gl.ARRAY_BUFFER, tex_coords_vbo);
-  gl.vertexAttribPointer(loc_tex_coords, 2, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(loc_tex_coords);
+  gl.vertexAttribPointer(Attrib.TEX_COORDS, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(Attrib.TEX_COORDS);
 
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
   gl.bindBuffer(gl.ARRAY_BUFFER, null);
-  gl.disableVertexAttribArray(loc_tex_coords);
-  gl.disableVertexAttribArray(loc_position);
+  gl.disableVertexAttribArray(Attrib.TEX_COORDS);
+  gl.disableVertexAttribArray(Attrib.POSITION);
   gl.bindTexture(gl.TEXTURE_2D, null);
   gl.disable(gl.TEXTURE_2D);
   gl.useProgram(null);
-};
-
-function createShaderProgram(gl) {
-  var vertexSrc =
-    "attribute vec2 a_position;" +
-    "attribute vec2 a_tex_coords;" +
-    "varying vec2 v_tex_coords;" +
-    "void main() {" +
-      "v_tex_coords = a_tex_coords;" +
-      "gl_Position = vec4(a_position, 0.0, 1.0);" +
-    "}";
-  var fragmentSrc =
-    "precision mediump float;" +
-    "uniform sampler2D u_texture;" +
-    "varying vec2 v_tex_coords;" +
-    "void main() {" +
-      "gl_FragColor = texture2D(u_texture, v_tex_coords);" +
-    "}";
-
-  var shaderProgram = gl.createProgram();
-
-  var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexSrc);
-  var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentSrc);
-
-  gl.attachShader(shaderProgram, vertexShader);
-  gl.attachShader(shaderProgram, fragmentShader);
-
-  gl.linkProgram(shaderProgram);
-  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    window.alert("Program linking error: " +
-                 gl.getProgramInfoLog(shaderProgram));
-  }
-
-  gl.deleteShader(vertexShader);
-  gl.deleteShader(fragmentShader);
-
-  return shaderProgram;
-};
-
-function createShader(gl, type, src) {
-  var shader = gl.createShader(type);
-  gl.shaderSource(shader, src);
-  gl.compileShader(shader);
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    window.alert("Shader compilation error: " + gl.getShaderInfoLog(shader));
-  }
-  return shader;
 };
